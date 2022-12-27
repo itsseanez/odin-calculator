22df7e7cfd4dc4da0f439e0bee7f9d78b0e55152
Sean Ezenwa <seanezenwa@gmail.com>
2022-12-26 21:13:45 -0500

    Complete operations methods

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+let operation = '';
 const add= (a, b) => a + b;
 const subtract= (a, b) => a - b;
 const multiply= (a, b) => a * b;
 const divide= (a, b) => a / b;
+function operate(operation, a, b) {
+    if(operation === '+') {
+        return add(a, b);
+    }
+    else if(operation === '-') {
+        return subtract(a, b);
+    }
+    else if(operation === '*') {
+        return multiply(a, b);
+    }
+    else if(operation === '/') {
+        return divide(a, b);
+    }
+}
+console.log(divide(30,5));
+console.log(operate('+', 30, 5));
